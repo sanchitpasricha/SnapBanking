a9d7d86d9b2a8a98c25072adf1ebc9ce13253f60
sanchitpasricha <sanchit0229@gmail.com>
2023-11-27 20:18:51 +0530

    setting up transactions dates

--- script.js
+++ script.js
@@ -8,14 +8,14 @@ const account1 = {
   pin: 1111,
 
   movementsDates: [
-    '2019-11-18T21:31:17.178Z',
-    '2019-12-23T07:42:02.383Z',
-    '2020-01-28T09:15:04.904Z',
-    '2020-04-01T10:17:24.185Z',
-    '2020-05-08T14:11:59.604Z',
-    '2020-05-27T17:01:17.194Z',
-    '2020-07-11T23:36:17.929Z',
-    '2020-07-12T10:51:36.790Z',
+    '2023-10-19T10:51:36.790Z',
+    '2023-10-20T23:36:17.929Z',
+    '2023-10-24T17:01:17.194Z',
+    '2023-10-27T14:11:59.604Z',
+    '2023-10-30T10:17:24.185Z',
+    '2023-11-18T21:31:17.178Z',
+    '2023-11-23T07:42:02.383Z',
+    '2023-11-28T09:15:04.904Z',
   ],
   currency: 'EUR',
   locale: 'pt-PT', // de-DE
@@ -28,14 +28,14 @@ const account2 = {
   pin: 2222,
 
   movementsDates: [
-    '2019-11-01T13:15:33.035Z',
-    '2019-11-30T09:48:16.867Z',
-    '2019-12-25T06:04:23.907Z',
-    '2020-01-25T14:18:46.235Z',
-    '2020-02-05T16:33:06.386Z',
-    '2020-04-10T14:43:26.374Z',
-    '2020-06-25T18:49:59.371Z',
-    '2020-07-26T12:01:20.894Z',
+    '2023-11-01T13:15:33.035Z',
+    '2023-11-30T09:48:16.867Z',
+    '2023-11-25T06:04:23.907Z',
+    '2023-11-25T14:18:46.235Z',
+    '2023-10-05T16:33:06.386Z',
+    '2023-10-10T14:43:26.374Z',
+    '2023-10-25T18:49:59.371Z',
+    '2023-10-26T12:01:20.894Z',
   ],
   currency: 'USD',
   locale: 'en-US',
@@ -69,6 +69,25 @@ const inputLoanAmount = document.querySelector('.form__input--loan-amount');
 const inputCloseUsername = document.querySelector('.form__input--user');
 const inputClosePin = document.querySelector('.form__input--pin');
 
+// Function to format date for app
+const formatMovementDate = function (date) {
+  const calcDaysPassed = (date1, date2) => {
+    return Math.round(Math.abs(date2 - date1) / (1000 * 60 * 60 * 24));
+  };
+
+  const daysPassed = calcDaysPassed(new Date(), date);
+
+  if (daysPassed === 0) return 'Today';
+  if (daysPassed === 1) return 'Yesterday';
+  if (daysPassed <= 7) return `${daysPassed} days ago`;
+  else {
+    const day = `${date.getDay()}`.padStart(2, 0);
+    const month = `${date.getMonth() + 1}`.padStart(2, 0);
+    const year = date.getFullYear();
+    return `${day}/${month}/${year}`;
+  }
+};
+
 // Function to push HTML elements to webPage
 const displayMovements = function (acc, sort = false) {
   containerMovements.innerHTML = '';
@@ -81,10 +100,7 @@ const displayMovements = function (acc, sort = false) {
     const type = mov > 0 ? 'deposit' : 'withdrawal';
 
     const date = new Date(acc.movementsDates[i]);
-    const day = `${date.getDay()}`.padStart(2, 0);
-    const month = `${date.getMonth() + 1}`.padStart(2, 0);
-    const year = date.getFullYear();
-    const displayDate = `${day}/${month}/${year}`;
+    const displayDate = formatMovementDate(date);
 
     const html = `<div class="movements__row">
     <div class="movements__type movements__type--${type}">${i + 1} ${type}</div>
@@ -150,11 +166,6 @@ const updateUI = function (acc) {
 
 // Adding funciotnality to login button
 let currentAccount;
-
-// Faking login
-// currentAccount = account1;
-// updateUI(currentAccount);
-// containerApp.style.opacity = 100;
 
 // Loging button
 btnLogin.addEventListener('click', function (e) {
